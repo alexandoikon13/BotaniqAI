baba1dd1bc4161e842b4d8a4b1a7b45d9e70a1aa
alexandoikon13 <alexandoikon@gmail.com>
2023-11-24 16:41:11 +0100

    added reference for model_utils

--- model/model_utils.py
+++ model/model_utils.py
@@ -1,6 +1,11 @@
 import torch
 
-def load_yolov8_model():
-    # Load the YOLOv8 model
-    model = torch.hub.load('ultralytics/yolov8', 'yolov8', pretrained=True)
+# Function to load the YOLOv8 model
+def load_model():
+    model = torch.hub.load('ultralytics/yolov8', 'yolov8', pretrained=True)  # Replace with the correct model
     return model
+
+# Function to run inference
+def run_inference(model, image_path):
+    results = model(image_path)
+    return results
