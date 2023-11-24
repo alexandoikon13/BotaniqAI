f42734a38b074e13168804e575d33f89161059c5
alexandoikon13 <alexandoikon@gmail.com>
2023-11-24 17:30:15 +0100

    updated the code to include better functionality

--- app/database.py
+++ app/database.py
@@ -12,3 +12,13 @@ def get_cassandra_session():
 def insert_image_data(session, image_id, image_data):
     cql = "INSERT INTO your_table (image_id, data) VALUES (%s, %s)"
     session.execute(cql, (image_id, image_data))
+
+def get_image_data(session, image_id):
+    cql = "SELECT data FROM your_table WHERE image_id = %s"
+    row = session.execute(cql, (image_id,)).one()
+    if row:
+        return row.data
+    else:
+        return None
+
+# More CRUD operations as needed...
