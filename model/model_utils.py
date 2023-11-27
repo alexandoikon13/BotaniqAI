800dcdb1d54d0138bed53d4e35b03307a583a913
alexandoikon13 <alexandoikon@gmail.com>
2023-11-27 13:50:44 +0100

    adjusted the file for yolov8 and Cloudcube

--- model/model_utils.py
+++ model/model_utils.py
@@ -1,24 +1,35 @@
+import boto3
 import torch
 from PIL import Image
 from io import BytesIO
+from ultralytics import YOLO
+import uuid
+
 
 # Function to load the YOLOv8 model
 def load_yolov8_model():
-    model = torch.hub.load('ultralytics/yolov8', 'yolov8', pretrained=True)
+    # Load a pretrained YOLO model (recommended for training)
+    model = YOLO('yolov8n.pt')
     return model
 
-# Function to run inference
-def run_inference(model, file_stream):
+# Function to run inference and save results in Cloudcube
+def run_inference_and_save(model, file_stream, cloudcube_url):
     # Convert file stream to PIL Image
     image = Image.open(BytesIO(file_stream.read()))
 
     # Perform inference
     results = model(image)
 
-    # Process the results (e.g., draw bounding boxes)
-    # This part depends on how you want to display the results.
-    # For simplicity, let's just save the results as an image.
-    results_image_path = 'path/to/save/results.jpg'
-    results.save(results_image_path)
+    # Save results as an image locally
+    local_results_path = f"results/{str(uuid.uuid4())}.jpg"
+    results.save(local_results_path)
 
-    return results_image_path
+    # Upload the results to Cloudcube
+    s3 = boto3.client('s3')
+    bucket_name = cloudcube_url.split('.')[0].split('//')[1]
+    object_name = f"processed/{str(uuid.uuid4())}.jpg"
+    s3.upload_file(local_results_path, bucket_name, object_name)
+
+    # Return the URL of the saved image in Cloudcube
+    cloudcube_results_url = f"{cloudcube_url}{object_name}"
+    return cloudcube_results_url
